14f715693d76a87b270c39b9a53ea69f25af6ef8
Ivan Garcia <ivangarciaharriague@gmail.com>
2022-09-26 17:34:28 -0300

    transform enhancements

--- pages/transform.jsx
+++ pages/transform.jsx
@@ -4,22 +4,27 @@ import styles from "../styles/anims.module.css";
 
 const Transform = () => {
   const x = useMotionValue(0);
-  const backgroundColor = useTransform(
+  const background = useTransform(
     x,
     [-200, 0, 200],
-    ["#0f0", "#f00", "#00f"]
+    [ "linear-gradient(180deg, #ff008c 0%, rgb(211, 9, 225) 100%)",
+      "linear-gradient(180deg, #7700ff 0%, rgb(68, 0, 255) 100%)",
+      "linear-gradient(180deg, rgb(230, 255, 0) 0%, rgb(3, 209, 0) 100%)"
+    ]
   );
 
   return (
-    <div className='container'>
+    <motion.div className='container' style={{ background }}>
       <h2 className={styles.title}>Transform on drag</h2>
       <motion.div
         className={styles.box}
-        style={{ x, backgroundColor }}
+        style={{ x }}
         drag='x'
         dragConstraints={{ left: -200, right: 200 }}
+        whileHover={{ scale: 1.1 }}
+        whileDrag={{ scale: 0.9 }}
       />
-    </div>
+    </motion.div>
   );
 };
 
